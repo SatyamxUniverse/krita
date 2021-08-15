b0636c63a5ce946a3286dfc839efeb17edfd6270
Tanmay Chavan <tanmaychavan2001@gmail.com>
2021-08-15 19:47:48 +0530

    Added files required for improved clipping of vector shapes. The documentation is mostly done, and so are the unit tests. It is not yet integrated with Krita intersection finding routine.

---
+++ plugins/tools/defaulttool/defaulttool/kisbooleanoperations.h
@@ -0,0 +1,82 @@
+/*
+ *  SPDX-FileCopyrightText: 2021 Tanmay Chavan <earendil01tc@gmail.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef KISBOOLEANOPERATIONS_H
+#define KISBOOLEANOPERATIONS_H
+
+#include "kisintersectionfinder.h"
+#include "kispathclipper.h"
+#include <QPainterPath>
+
+/*
+ * This class contains the functions needed for boolean operations on vector
+ * shapes. It mostly is a wrapper class which uses the other functions
+ * together to give final clipped shapes.
+ *
+ * Currently, the clipping algorithm is buggy. It is able to find all the
+ * intersection points accurately between two shapes containing curves and also
+ * splits the elements about the points of intersection. However clipping using
+ * KisPathClipper doesn't work as expected, and gives some extra edges.
+ *
+ * The current approach is to traverse QPainterPath and substitute the
+ * curves we have calculated. This approach, has been implemented in
+ * KisIntersectionFinder::resubstituteCurves() works for almost all cases
+ * but might generate a few extra lines while carrying subtraction on certain
+ * shapes (containing curves.)
+ */
+
+class KisBooleanOperations {
+
+public:
+
+    KisBooleanOperations();
+    ~KisBooleanOperations();
+
+    /*
+     * returns the union of two QPainterPaths
+     */
+    QPainterPath unite(QPainterPath &sub, QPainterPath &clip);
+
+
+    /*
+     * returns the intersection of two QPainterPaths
+     */
+    QPainterPath intersect(QPainterPath &sub, QPainterPath &clip);
+
+
+    /*
+     * returns the difference of the second QPainterPath from the first.
+     */
+    QPainterPath subtract(QPainterPath &sub, QPainterPath &clip);
+
+
+    /*
+     * Unites the second QPainterPath with the first, and returns it as well.
+     */
+    QPainterPath uniteAndAdd(QPainterPath &sub, QPainterPath &clip);
+
+
+    /*
+     * Intersects the second QPainterPath with the first, and returns it as
+     * well.
+     */
+    QPainterPath intersectAndAdd( QPainterPath &sub, QPainterPath &clip );
+
+
+    /*
+     * Subtracts the second QPainterPath from the first, and then returns it
+     * as well.
+     */
+    QPainterPath subtractAndAdd( QPainterPath &sub, QPainterPath &clip );
+
+
+    /*
+     * test function to test various clipping operations.
+     */
+    QPainterPath testAdd();
+};
+
+#endif
---
+++ plugins/tools/defaulttool/tests/kisintersectionfindertest.h
@@ -0,0 +1,34 @@
+/*
+ *  SPDX-FileCopyrightText: 2021 Tanmay Chavan <earendil01tc@gmail.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef KISINTERSECTIONFINDERTEST_H
+#define KISINTERSECTIONFINDERTEST_H
+
+#include <QObject>
+
+class KisIntersectionFinderTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit KisIntersectionFinderTest(QObject *parent = nullptr);
+
+private slots:
+
+    void lineLineIntersectionTest();
+    void lineCurveIntersectionTest();
+    void curveCurveIntersectionTest();
+    void curveSelfIntersectionTest();
+
+    void QPainterPathCompatibilityTest();
+
+    void allIntersectionPointsTest();
+
+
+signals:
+
+};
+
+#endif // KISINTERSECTIONFINDERTEST_H
