9bfebdead6df710f08c8df0f5c7addab0378f8a1
L. E. Segovia <amy@amyspark.me>
2022-06-25 13:14:32 -0300

    Fix undeclared dependency of poppler on freetype

--- build-tools/windows/build.cmd
+++ build-tools/windows/build.cmd
@@ -953,11 +953,11 @@ echo.
 
 set EXT_TARGETS=patch zlib gettext openssl boost exiv2 fftw3 eigen3 jpeg lcms2
 set EXT_TARGETS=%EXT_TARGETS% ocio openexr png icoutils tiff gsl libraw
-set EXT_TARGETS=%EXT_TARGETS% giflib qt kwindowsystem drmingw poppler 
+set EXT_TARGETS=%EXT_TARGETS% giflib qt kwindowsystem drmingw
 set EXT_TARGETS=%EXT_TARGETS% python sip pyqt
 set EXT_TARGETS=%EXT_TARGETS% lzma quazip openjpeg libde265 libx265 libheif
 set EXT_TARGETS=%EXT_TARGETS% seexpr mypaint webp jpegxl xsimd
-set EXT_TARGETS=%EXT_TARGETS% freetype raqm unibreak fribidi fontconfig
+set EXT_TARGETS=%EXT_TARGETS% freetype poppler raqm unibreak fribidi fontconfig
 
 for %%a in (%EXT_TARGETS%) do (
     set TEST_HAS_TARGET=
